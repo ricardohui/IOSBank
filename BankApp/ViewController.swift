8fee63b1d1ecbcbed627abe01c77562d14c28049
Ricardo Hui <eehaha@gmail.com>
2020-05-27 08:10:47 +0800

    Initial Commit

---
+++ BankApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Ricardo on 27/5/2020.
+//  Copyright © 2020 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
